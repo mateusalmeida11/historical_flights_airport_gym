d92db9bd8d6d7f9bcf098a632380cbec3534de9c
Mateus Almeida <mateusalmeida881@gmail.com>
2025-10-02 16:50:19 -0300

    test: new - status code 200

---
+++ tests/utils/test_request.py
@@ -0,0 +1,16 @@
+from unittest.mock import MagicMock, patch
+from historical_flights_airport_gym.utils.get_data import get_data
+
+
+@patch("historical_flights_airport_gym.utils.get_data.Session.get")
+def test_status_200_get_request(mock_get):
+    mock_response = MagicMock()
+    json_str_response = '[{"sg_empresa_icao":"AAL","nm_empresa":"AMERICAN AIRLINES, INC.","nr_voo":"0904","cd_di":"0","cd_tipo_linha":"I","sg_equipamento_icao":"B788","nr_assentos_ofertados":"295","sg_icao_origem":"SBGL","nm_aerodromo_origem":"AEROPORTO INTERNACIONAL DO RIO DE JANEIRO (GALEÃO) - ANTONIO CARLOS JOBIM - RIO DE JANEIRO - RJ - BRASIL","dt_partida_prevista":"01/01/2025 23:55","dt_partida_real":"01/01/2025 23:50","sg_icao_destino":"KMIA","nm_aerodromo_destino":"MIAMI INTERNATIONAL AIRPORT - MIAMI, FLORIDA - ESTADOS UNIDOS DA AMÉRICA","dt_chegada_prevista":"02/01/2025 07:45","dt_chegada_real":"02/01/2025 08:11","ds_situacao_voo":"REALIZADO","ds_justificativa":"","dt_referencia":"01/01/2025","ds_situacao_partida":"Antecipado","ds_situacao_chegada":"Pontual"}]'
+    mock_response.status_code = 200
+    mock_response.json.return_value = json_str_response
+    mock_get.return_value = mock_response
+
+    response = get_data()
+
+    assert response.status_code == 200
+    assert response.json() is str
