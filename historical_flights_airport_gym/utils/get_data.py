b3bf82d83d3fec4b7611b024c140a656d002defd
Mateus Almeida <mateusalmeida881@gmail.com>
2025-10-05 10:06:23 -0300

    refactor: criacao de uma classe de erro

--- historical_flights_airport_gym/utils/get_data.py
+++ historical_flights_airport_gym/utils/get_data.py
@@ -1,6 +1,14 @@
 import requests
 
 
+class RequestError(Exception):
+    def __init__(self, message, status_code=None, endpoint=None, response_body=None):
+        super().__init__(message)
+        self.status_code = status_code
+        self.endpoint = endpoint
+        self.response_body = response_body
+
+
 def get_data(url, params):
     http = requests.Session()
     response = http.get(url=url, params=params)
