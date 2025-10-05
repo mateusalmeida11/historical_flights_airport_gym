1f36f8c87012027316cf2946964d37587698235a
Mateus Almeida <mateusalmeida881@gmail.com>
2025-10-05 10:19:30 -0300

    refactor: implementacao de retry e excecao

--- historical_flights_airport_gym/utils/get_data.py
+++ historical_flights_airport_gym/utils/get_data.py
@@ -1,4 +1,6 @@
 import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
 
 
 class RequestError(Exception):
@@ -10,6 +12,23 @@ class RequestError(Exception):
 
 
 def get_data(url, params):
+    retry_strategy = Retry(
+        total=3,
+        status_forcelist=[408, 429, 500, 502, 503, 504],
+        allowed_methods=["GET"],
+        backoff_factor=1,
+    )
+    adapter = HTTPAdapter(max_retries=retry_strategy)
     http = requests.Session()
-    response = http.get(url=url, params=params)
-    return response
+    http.mount("https://", adapter)
+    http.mount("http://", adapter)
+    try:
+        response = http.get(url=url, params=params)
+        response.raise_for_status()
+        return response
+    except requests.exceptions.HTTPError as e:
+        raise RequestError(
+            f"HTTP Error {e.response.status_code}",
+            status_code=e.response.status_code,
+            response_body=e.response.text,
+        ) from e
