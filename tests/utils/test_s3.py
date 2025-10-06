05a160c55ace78319464c49a0da9c6d0412a3478
Mateus Almeida <mateusalmeida881@gmail.com>
2025-10-06 16:41:22 -0300

    refactor: criacao do modulo de execao para upload error e bloco try-except

--- tests/utils/test_s3.py
+++ tests/utils/test_s3.py
@@ -1,9 +1,10 @@
 import json
 
 import boto3
+import pytest
 from moto import mock_aws
 
-from historical_flights_airport_gym.utils.aws.S3 import S3
+from historical_flights_airport_gym.utils.aws.S3 import S3, S3UploadError
 
 
 @mock_aws
@@ -45,3 +46,46 @@ def test_upload_s3_success():
     status_code = response["ResponseMetadata"]["HTTPStatusCode"]
     assert status_code == 200
     assert "ETag" in response
+
+
+@mock_aws
+def test_upload_s3_inexistente():
+    bucket_name = "etl-brazilian-flights"
+
+    boto3.client("s3", region_name="us-east-1")
+    key = "flights/flights-brazil/2025_10_06_123456789_0.json"
+
+    body = [
+        {
+            "sg_empresa_icao": "AAL",
+            "nm_empresa": "AMERICAN AIRLINES, INC.",
+            "nr_voo": "0904",
+            "cd_di": "0",
+            "cd_tipo_linha": "I",
+            "sg_equipamento_icao": "B788",
+            "nr_assentos_ofertados": "295",
+            "sg_icao_origem": "SBGL",
+            "nm_aerodromo_origem": "AEROPORTO INTERNACIONAL DO RIO DE JANEIRO (GALEÃO) - ANTONIO CARLOS JOBIM - RIO DE JANEIRO - RJ - BRASIL",
+            "dt_partida_prevista": "01/01/2025 23:55",
+            "dt_partida_real": "01/01/2025 23:50",
+            "sg_icao_destino": "KMIA",
+            "nm_aerodromo_destino": "MIAMI INTERNATIONAL AIRPORT - MIAMI, FLORIDA - ESTADOS UNIDOS DA AMÉRICA",
+            "dt_chegada_prevista": "02/01/2025 07:45",
+            "dt_chegada_real": "02/01/2025 08:11",
+            "ds_situacao_voo": "REALIZADO",
+            "ds_justificativa": "",
+            "dt_referencia": "01/01/2025",
+            "ds_situacao_partida": "Antecipado",
+            "ds_situacao_chegada": "Pontual",
+        }
+    ]
+
+    jsonData = json.dumps(body, indent=4)
+
+    with pytest.raises(S3UploadError) as excinfo:
+        s3 = S3(bucket_name=bucket_name)
+        s3.upload_file(data=jsonData, key=key)
+
+    e = excinfo.value
+    assert e.status_code == 404
+    assert e.message == "The specified bucket does not exist"
