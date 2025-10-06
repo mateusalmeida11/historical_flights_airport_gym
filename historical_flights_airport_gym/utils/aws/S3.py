7da9e1e016688ea8341d976e45b2c86eef35c97e
Mateus Almeida <mateusalmeida881@gmail.com>
2025-10-06 16:41:57 -0300

    refactor: criacao do modulo de excecao e bloco try-except

--- historical_flights_airport_gym/utils/aws/S3.py
+++ historical_flights_airport_gym/utils/aws/S3.py
@@ -1,4 +1,12 @@
 import boto3
+from botocore.exceptions import ClientError
+
+
+class S3UploadError(Exception):
+    def __init__(self, message, status_code=None):
+        super().__init__(message)
+        self.message = message
+        self.status_code = status_code
 
 
 class S3:
@@ -7,7 +15,16 @@ class S3:
         self.bucket_name = bucket_name
 
     def upload_file(self, data, key):
-        response = self.s3_client.put_object(
-            Body=data, Key=key, Bucket=self.bucket_name, ContentType="application/json"
-        )
-        return response
+        try:
+            response = self.s3_client.put_object(
+                Body=data,
+                Key=key,
+                Bucket=self.bucket_name,
+                ContentType="application/json",
+            )
+            return response
+        except ClientError as e:
+            raise S3UploadError(
+                e.response["Error"]["Message"],
+                status_code=e.response["ResponseMetadata"]["HTTPStatusCode"],
+            ) from e
