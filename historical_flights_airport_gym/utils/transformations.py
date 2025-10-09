eeb3802afb4d31ce4a8b05cbf07a6ce599f086f0
Mateus Almeida <mateusalmeida881@gmail.com>
2025-10-09 15:00:14 -0300

    refactor: criacao de excecao para Json

--- historical_flights_airport_gym/utils/transformations.py
+++ historical_flights_airport_gym/utils/transformations.py
@@ -3,6 +3,18 @@ import json
 import requests
 
 
+class JsonProcessingError(Exception):
+    def __init__(self, message, status_code=None, response_body=None):
+        super().__init__(message)
+        self.status_code = status_code
+        self.response_body = response_body
+
+
 def from_str_to_json(response: requests.Response):
-    jsonStr = response.json()
-    return json.loads(jsonStr)
+    try:
+        jsonStr = response.json()
+        return json.loads(jsonStr)
+    except requests.exceptions.JSONDecodeError as e:
+        raise JsonProcessingError(
+            "Erro ao Processar Json", status_code=500, response_body=e
+        ) from e
