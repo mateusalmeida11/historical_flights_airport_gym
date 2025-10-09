d37e4cba107bb5b01ec26aaf956f53716b4d22af
Mateus Almeida <mateusalmeida881@gmail.com>
2025-10-09 17:04:53 -0300

    test: new - upload success

---
+++ tests/anac/test_lambda_handler.py
@@ -0,0 +1,35 @@
+from unittest.mock import MagicMock, patch
+
+import boto3
+from moto import mock_aws
+
+from historical_flights_airport_gym.conectores.anac.lambda_handler import lambda_handler
+
+
+@mock_aws
+@patch("historical_flights_airport_gym.utils.get_data.requests.Session.get")
+def test_success_connect_data_and_upload_s3(mock_get):
+    mock_response = MagicMock()
+    json_str_response = '[{"sg_empresa_icao":"AAL","nm_empresa":"AMERICAN AIRLINES, INC.","nr_voo":"0904","cd_di":"0","cd_tipo_linha":"I","sg_equipamento_icao":"B788","nr_assentos_ofertados":"295","sg_icao_origem":"SBGL","nm_aerodromo_origem":"AEROPORTO INTERNACIONAL DO RIO DE JANEIRO (GALEÃO) - ANTONIO CARLOS JOBIM - RIO DE JANEIRO - RJ - BRASIL","dt_partida_prevista":"01/01/2025 23:55","dt_partida_real":"01/01/2025 23:50","sg_icao_destino":"KMIA","nm_aerodromo_destino":"MIAMI INTERNATIONAL AIRPORT - MIAMI, FLORIDA - ESTADOS UNIDOS DA AMÉRICA","dt_chegada_prevista":"02/01/2025 07:45","dt_chegada_real":"02/01/2025 08:11","ds_situacao_voo":"REALIZADO","ds_justificativa":"","dt_referencia":"01/01/2025","ds_situacao_partida":"Antecipado","ds_situacao_chegada":"Pontual"}]'
+    mock_response.status_code = 200
+    mock_response.json.return_value = json_str_response
+    mock_get.json.return_value = mock_response
+
+    bucket_name = "etl-brazilian-flights"
+
+    client = boto3.client("s3", region_name="us-east-1")
+    client.creeate_bucket(Bucket=bucket_name)
+
+    event = {
+        "layer": "bronze",
+        "bucket": bucket_name,
+        "dt_voo": "01012025",
+    }
+
+    context = {}
+
+    result = lambda_handler(event=event, context=context)
+
+    assert result["status"] == "success"
+    assert result["bucket"] == bucket_name
+    assert result["s3_response"]["status_code"] == 200
