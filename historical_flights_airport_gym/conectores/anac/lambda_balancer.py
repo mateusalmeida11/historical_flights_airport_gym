676cd8bd1ef8d4242d008a7a19444f44ee7c962f
Mateus Almeida <mateusalmeida881@gmail.com>
2025-10-11 18:00:13 -0300

    refactor: levantar excecao com erro de transformacao de data

--- historical_flights_airport_gym/conectores/anac/lambda_balancer.py
+++ historical_flights_airport_gym/conectores/anac/lambda_balancer.py
@@ -1,3 +1,9 @@
+from historical_flights_airport_gym.utils.transformations import (
+    DateTransformationError,
+    from_str_to_datetime,
+)
+
+
 def lambda_handler(event, context):
     start_str_date = event.get("date_start")
     end_str_date = event.get("date_end")
@@ -8,4 +14,15 @@ def lambda_handler(event, context):
             "type": "InvalidDateLenghtError",
             "status_code": 500,
             "message": "Objeto de Data Deve Conter Exatamente 8 caracteres  no formato DDMMYYY",
+        }
+
+    try:
+        from_str_to_datetime(date=start_str_date)
+        from_str_to_datetime(date=end_str_date)
+    except DateTransformationError as e:
+        return {
+            "status": "error",
+            "type": "DateTransformationError",
+            "status_code": 500,
+            "message": str(e),
         }
