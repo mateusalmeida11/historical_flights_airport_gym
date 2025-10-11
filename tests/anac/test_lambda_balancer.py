2436985ba38986b9858c3f4d501834d68b28c59a
Mateus Almeida <mateusalmeida881@gmail.com>
2025-10-11 17:30:22 -0300

    test: new - erro de data fora do intervalo

--- tests/anac/test_lambda_balancer.py
+++ tests/anac/test_lambda_balancer.py
@@ -1,9 +1,14 @@
 from datetime import datetime
 
+import pytest
+
 from historical_flights_airport_gym.conectores.anac.lambda_balancer import (
     lambda_handler,
 )
-from historical_flights_airport_gym.utils.transformations import from_str_to_datetime
+from historical_flights_airport_gym.utils.transformations import (
+    DateOutRangeError,
+    from_str_to_datetime,
+)
 
 
 def test_date_initial_greater_than_8_dig():
@@ -30,3 +35,14 @@ def test_funcao_de_transformacao_str_para_data_funciona():
     obj_convertido = from_str_to_datetime(date=start_date)
 
     assert isinstance(obj_convertido, datetime)
+
+
+def test_funcao_de_transformacao_str_para_data_erro_date_out_range():
+    start_date = "33012025"
+
+    with pytest.raises(DateOutRangeError) as excinfo:
+        from_str_to_datetime(date=start_date)
+
+    e = excinfo.value
+
+    assert e.message == f"Data {start_date} fora do intervalo do mes"
