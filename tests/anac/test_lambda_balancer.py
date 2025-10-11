dc21f12e5620bc6cc6276779c2b4c17ccd36dbf7
Mateus Almeida <mateusalmeida881@gmail.com>
2025-10-11 15:43:38 -0300

    test: new - validacao de str maior do que permitido

---
+++ tests/anac/test_lambda_balancer.py
@@ -0,0 +1,22 @@
+from historical_flights_airport_gym.conectores.anac.lambda_balancer import (
+    lambda_handler,
+)
+
+
+def test_date_initial_greater_than_8_dig():
+    start_str_date = "01012025"
+    end_str_date = "320120253"
+
+    event = {"date_start": start_str_date, "date_end": end_str_date}
+
+    context = {}
+
+    result = lambda_handler(event=event, context=context)
+
+    assert result["status"] == "error"
+    assert result["type"] == "InvalidDateLenghtError"
+    assert result["status_code"] == 500
+    assert (
+        result["message"]
+        == "Objeto de Data Deve Conter Exatamente 8 caracteres  no formato DDMMYYY"
+    )
