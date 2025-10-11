4ae735843b10c57112e6ff5d0e43c538862a51d5
Mateus Almeida <mateusalmeida881@gmail.com>
2025-10-11 14:10:26 -0300

    refactor: adicao da excecao gerada pela api

--- historical_flights_airport_gym/conectores/anac/lambda_handler.py
+++ historical_flights_airport_gym/conectores/anac/lambda_handler.py
@@ -2,34 +2,42 @@ import json
 
 from historical_flights_airport_gym.utils.aws.S3 import S3
 from historical_flights_airport_gym.utils.build_path import path_file_raw
-from historical_flights_airport_gym.utils.get_data import get_data
+from historical_flights_airport_gym.utils.get_data import RequestError, get_data
 from historical_flights_airport_gym.utils.transformations import from_str_to_json
 
 s3 = S3()
 
 
 def lambda_handler(event, context):
-    layer = event.get("layer")
-    bucket = event.get("bucket")
-    dt_voo = event.get("dt_voo")
+    try:
+        layer = event.get("layer")
+        bucket = event.get("bucket")
+        dt_voo = event.get("dt_voo")
 
-    file_name = path_file_raw()
-    key = f"{layer}/flights/{file_name}.json"
+        file_name = path_file_raw()
+        key = f"{layer}/flights/{file_name}.json"
 
-    url = "https://sas.anac.gov.br/sas/vra_api/vra/data?"
-    params = {"dt_voo": dt_voo}
+        url = "https://sas.anac.gov.br/sas/vra_api/vra/data?"
+        params = {"dt_voo": dt_voo}
 
-    response = get_data(url=url, params=params)
-    jsonObj = from_str_to_json(response=response)
-    jsonData = json.dumps(jsonObj, indent=4)
+        response = get_data(url=url, params=params)
+        jsonObj = from_str_to_json(response=response)
+        jsonData = json.dumps(jsonObj, indent=4)
 
-    response_s3 = s3.upload_file(bucket=bucket, data=jsonData, key=key)
+        response_s3 = s3.upload_file(bucket=bucket, data=jsonData, key=key)
 
-    return {
-        "status": "success",
-        "bucket": bucket,
-        "key": key,
-        "s3_response": {
-            "status_code": response_s3["ResponseMetadata"]["HTTPStatusCode"]
-        },
-    }
+        return {
+            "status": "success",
+            "bucket": bucket,
+            "key": key,
+            "s3_response": {
+                "status_code": response_s3["ResponseMetadata"]["HTTPStatusCode"]
+            },
+        }
+
+    except RequestError as e:
+        return {
+            "status": "error",
+            "type": "APIError",
+            "status_code": str(e),
+        }
