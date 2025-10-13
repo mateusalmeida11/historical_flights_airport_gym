9ade528f3dd218d66c85ccec8c11988eb86af186
Mateus Almeida <mateusalmeida881@gmail.com>
2025-10-13 15:59:47 -0300

    file: correcao - variavel values para value

--- ecr-module/outputs.tf
+++ ecr-module/outputs.tf
@@ -1,4 +1,4 @@
 output "ecr_repository_url" {
     description = "URL de saida do repositorio ECR"
-    values = aws_ecr_repository.docker_lambda_flights.repository_url
+    value = aws_ecr_repository.docker_lambda_flights.repository_url
 }
