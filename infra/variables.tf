6233e6d4fd96fa10ab412bef7f4ee25df975ed0a
Mateus Almeida <mateusalmeida881@gmail.com>
2025-12-19 15:44:17 -0300

    file: criacao do arquivo de variaveis

---
+++ infra/variables.tf
@@ -0,0 +1,9 @@
+variable "ecr_repository_url" {
+    description = "Valor Proveniente do GithubActions"
+    type = string
+}
+
+variable "image_tag" {
+    description = "Valor Gerado no GithubActions"
+    type = string
+}
