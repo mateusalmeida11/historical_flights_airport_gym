d418b9be1f94548c12f220c52414478bbecf8042
Mateus Almeida <mateusalmeida881@gmail.com>
2025-12-19 15:03:13 -0300

    file: correcao na declaracao de image_uri

--- infra/modules/lambda/main.tf
+++ infra/modules/lambda/main.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "lambda_extract_flights" {
     function_name = var.lambda_extract_name
     package_type = "Image"
     role = var.lambda_execution_role_flights_arn
-    image_uri = "${var.ecr_repository_url}:{var.image_tag}"
+    image_uri = "${var.ecr_repository_url}:${var.image_tag}"
 
     image_config {
         command = [var.command_aws_lambda_flights]
