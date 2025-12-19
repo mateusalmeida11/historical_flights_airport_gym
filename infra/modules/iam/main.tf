acdb2a6e5d85d29e8ecafbf2f8a5c1c77bb65413
Mateus Almeida <mateusalmeida881@gmail.com>
2025-12-19 16:27:33 -0300

    file: correcao de arquivo

--- infra/modules/iam/main.tf
+++ infra/modules/iam/main.tf
@@ -4,7 +4,7 @@ resource "aws_iam_role" "role_lambda_flights" {
     name=var.iam_role_name_lambda
 
     assume_role_policy = jsonencode({
-        version="2012-10-17"
+        Version="2012-10-17"
         Statement = [
             {
                 Action = "sts:AssumeRole",
