a529258e0b6455ac66de18d941e21b65fb9524f9
Mateus Almeida <mateusalmeida881@gmail.com>
2026-01-14 15:18:25 -0300

    teste: refactor a criacao do cliente s3

--- tests/data_quality_checks/test_quality.py
+++ tests/data_quality_checks/test_quality.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 import boto3
@@ -23,44 +24,51 @@ def teste_data_quality_staging_to_bronze_source_anac_success():
     # 1. Mockar Bucket S3 na AWS
     bucket_name = "mateus-us-east-1-etl-flights"
     key = "staging/2025_10_06_123456789_0.json"
-    client = boto3.client("s3", region_name="us-east-1")
 
-    client.create_bucket(Bucket=bucket_name)
+    s3_client = boto3.client(
+        "s3",
+        endpoint_url="http://localhost:4566",
+        aws_access_key_id="test",
+        aws_secret_access_key="test",
+        region_name="us-east-1",
+    )
+
+    s3_client.create_bucket(Bucket=bucket_name)
 
     # 2. Simular Arquivo
     body = {
         "metada": {},
-        "content": {
-            [
-                {
-                    "sg_empresa_icao": "AAL",
-                    "nm_empresa": "AMERICAN AIRLINES, INC.",
-                    "nr_voo": "0904",
-                    "cd_di": "0",
-                    "cd_tipo_linha": "I",
-                    "sg_equipamento_icao": "B772",
-                    "nr_assentos_ofertados": "288",
-                    "sg_icao_origem": "SBGL",
-                    "nm_aerodromo_origem": "AEROPORTO INTERNACIONAL DO RIO DE JANEIRO (GALE\u00c3O) - ANTONIO CARLOS JOBIM - RIO DE JANEIRO - RJ - BRASIL",
-                    "dt_partida_prevista": "31/10/2025 22:55",
-                    "dt_partida_real": "31/10/2025 22:46",
-                    "sg_icao_destino": "KMIA",
-                    "nm_aerodromo_destino": "MIAMI INTERNATIONAL AIRPORT - MIAMI, FLORIDA - ESTADOS UNIDOS DA AM\u00c9RICA",
-                    "dt_chegada_prevista": "01/11/2025 07:30",
-                    "dt_chegada_real": "01/11/2025 07:09",
-                    "ds_situacao_voo": "REALIZADO",
-                    "ds_justificativa": "",
-                    "dt_referencia": "31/10/2025",
-                    "ds_situacao_partida": "Antecipado",
-                    "ds_situacao_chegada": "Antecipado",
-                },
-            ]
-        },
+        "content": [
+            {
+                "sg_empresa_icao": "AAL",
+                "nm_empresa": "AMERICAN AIRLINES, INC.",
+                "nr_voo": "0904",
+                "cd_di": "0",
+                "cd_tipo_linha": "I",
+                "sg_equipamento_icao": "B772",
+                "nr_assentos_ofertados": "288",
+                "sg_icao_origem": "SBGL",
+                "nm_aerodromo_origem": "AEROPORTO INTERNACIONAL DO RIO DE JANEIRO (GALEÃO) - ANTONIO CARLOS JOBIM - RIO DE JANEIRO - RJ - BRASIL",
+                "dt_partida_prevista": "20/11/2025 23:55",
+                "dt_partida_real": "20/11/2025 23:48",
+                "sg_icao_destino": "KMIA",
+                "nm_aerodromo_destino": "MIAMI INTERNATIONAL AIRPORT - MIAMI, FLORIDA - ESTADOS UNIDOS DA AMÉRICA",
+                "dt_chegada_prevista": "21/11/2025 08:40",
+                "dt_chegada_real": "21/11/2025 08:31",
+                "ds_situacao_voo": "REALIZADO",
+                "ds_justificativa": "",
+                "dt_referencia": "20/11/2025",
+                "ds_situacao_partida": "Antecipado",
+                "ds_situacao_chegada": "Antecipado",
+            },
+        ],
     }
 
+    jsonData = json.dumps(body, indent=4)
+
     # 2. Upload para o S3
-    s3 = S3()
-    s3.upload_file(data=body, bucket=bucket_name, key=key)
+    s3 = S3(s3_client=s3_client)
+    s3.upload_file(data=jsonData, bucket=bucket_name, key=key)
 
     # 3. Simulacao do Event da Camada Staging
     event = {
