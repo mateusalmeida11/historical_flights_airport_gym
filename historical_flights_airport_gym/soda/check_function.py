d02dcdbc4cf3861939b73c941182d3dd96a5ec6c
Mateus Almeida <mateusalmeida881@gmail.com>
2026-01-15 10:22:51 -0300

    refactor: adicionando conexao ao banco de dados

--- historical_flights_airport_gym/soda/check_function.py
+++ historical_flights_airport_gym/soda/check_function.py
@@ -1,2 +1,17 @@
-def check():
+import duckdb
+
+
+def check(event, context):
+    conn = duckdb.connect(":memory")
+    conn.execute("INSTALL httpfs;")
+    conn.execute("LOAD httpfs;")
+    conn.execute(
+        """
+                 CREATE OR REPLACE SECRET secret (
+                 TYPE s3,
+                 PROVIDER credential_chain
+                 );
+                 """
+    )
+
     pass
