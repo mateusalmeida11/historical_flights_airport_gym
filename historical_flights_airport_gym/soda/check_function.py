2a2b6f07167a7c226e558053c6bddaaa77c5ba09
Mateus Almeida <mateusalmeida881@gmail.com>
2026-01-15 15:10:00 -0300

    refactor: implementacao da query no duckdb

--- historical_flights_airport_gym/soda/check_function.py
+++ historical_flights_airport_gym/soda/check_function.py
@@ -13,5 +13,22 @@ def check(event, context):
                  );
                  """
     )
-
-    pass
+    # criar path para o bucket
+    bucket = event.get("bucket")
+    key = event.get("key")
+    uri_bucket = f"s3://{bucket}/{key}"
+    # fazer consulta SQL
+    query = f"""
+    CREATE TABLE IF NOT EXISTS flights AS
+    SELECT
+        content.*
+    FROM
+        (
+            SELECT
+                unnest(content) AS content
+            FROM
+                read_json('{uri_bucket}')
+        ) AS json_content;
+    """
+    conn.execute(query)
+    return {"status": "success"}
