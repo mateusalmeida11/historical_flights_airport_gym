c5ae3c0fb11b308f6aceb728489a9761de93b9fd
Mateus Almeida <mateusalmeida881@gmail.com>
2026-01-15 13:34:17 -0300

    teste: new - validando tipo da conexao com o DuckDB

---
+++ tests/utils/test_duckdb.py
@@ -0,0 +1,20 @@
+import boto3
+from duckdb import DuckDBPyConnection
+
+from historical_flights_airport_gym.utils.duckdb.connect_duckdb import DuckDBManager
+
+
+def create_conexao_localstack():
+    return boto3.client(
+        "s3",
+        endpoint_url="http://localhost:4566",
+        aws_access_key_id="test",
+        aws_secret_access_key="test",
+        region_name="us-east-1",
+    )
+
+
+def test_conexao_duckdb_s3_success():
+    conn = DuckDBManager()
+
+    isinstance(conn, DuckDBPyConnection)
