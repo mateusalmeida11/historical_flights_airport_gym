75dda26722087cb37fe9099da2a22d17c9bf9fc1
Mateus Almeida <mateusalmeida881@gmail.com>
2026-01-15 14:23:53 -0300

    refactor: implementando message

--- historical_flights_airport_gym/utils/duckdb/connect_duckdb.py
+++ historical_flights_airport_gym/utils/duckdb/connect_duckdb.py
@@ -1,9 +1,10 @@
 import duckdb
 
 
-class DuckDBConnectionError(Exception):
-    def __init__(self, message, status_code=None):
+class DuckDBHTTPError(Exception):
+    def __init__(self, message):
         super().__init__(message)
+        self.message = message
 
 
 class DuckDBManager:
