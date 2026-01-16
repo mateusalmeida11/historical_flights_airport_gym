21dfe45d4085adfc7037ca1e264d2b8ced63dc71
Mateus Almeida <mateusalmeida881@gmail.com>
2026-01-16 16:34:25 -0300

    refactor: adaptando caminho do arquivo

--- historical_flights_airport_gym/soda/check_function.py
+++ historical_flights_airport_gym/soda/check_function.py
@@ -1,25 +1,30 @@
-import duckdb
+import json
+from pathlib import Path
+
+from soda.scan import Scan
+
+from historical_flights_airport_gym.utils.aws.S3 import S3
+from historical_flights_airport_gym.utils.build_path import path_file_raw
+from historical_flights_airport_gym.utils.duckdb.connect_duckdb import DuckDBManager
 
 
 def check(event, context):
-    conn = duckdb.connect(":memory")
-    conn.execute("INSTALL httpfs;")
-    conn.execute("LOAD httpfs;")
-    conn.execute(
-        """
-                 CREATE OR REPLACE SECRET secret (
-                 TYPE s3,
-                 PROVIDER credential_chain
-                 );
-                 """
-    )
-    # criar path para o bucket
     bucket = event.get("bucket")
     key = event.get("key")
-    uri_bucket = f"s3://{bucket}/{key}"
-    # fazer consulta SQL
+    table_name = event.get("table_name")
+    checks_subpath = event.get("checks_subpath")
+
+    root_path = str(Path(__file__).resolve().parent)
+
+    checks_path = f"{root_path}/sources/"
+
+    if checks_subpath:
+        checks_path += checks_subpath
+
+    s3_uri = f"s3://{bucket}/{key}"
+
     query = f"""
-    CREATE TABLE IF NOT EXISTS flights AS
+    CREATE OR REPLACE VIEW '{table_name}' AS
     SELECT
         content.*
     FROM
@@ -27,8 +32,36 @@ def check(event, context):
             SELECT
                 unnest(content) AS content
             FROM
-                read_json('{uri_bucket}')
-        ) AS json_content;
+                read_json('{s3_uri}')
+        );
     """
-    conn.execute(query)
-    return {"status": "success"}
+
+    db = DuckDBManager()
+    conn = db.conn
+    db.make_query(query)
+
+    layer = key.split("/")[0]
+
+    scan = Scan()
+    scan.set_verbose(True)
+    scan.add_duckdb_connection(conn)
+    scan.set_data_source_name("duckdb")
+    scan.add_sodacl_yaml_files(checks_path)
+
+    result = scan.execute()
+    jsonObj = scan.get_scan_results()
+
+    # create path to upload s3
+
+    file_name = path_file_raw()
+    key_log = f"logs/dq-checks/{layer}/{file_name}.json"
+
+    s3 = S3()
+    jsonData = json.dumps(jsonObj, indent=4)
+    response_s3 = s3.upload_file(bucket=bucket, data=jsonData, key=key_log)
+    return {
+        "status_soda": result,
+        "bucket": bucket,
+        "path_file_check": f"{bucket}/{key}",
+        "s3": {"status_code": response_s3["ResponseMetadata"]["HTTPStatusCode"]},
+    }
