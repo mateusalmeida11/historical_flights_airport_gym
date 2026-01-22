45dedeebaf85dcf74a85cf4b57b031f1295ebd64
Mateus Almeida <mateusalmeida881@gmail.com>
2026-01-22 16:40:14 -0300

    refactor: criacao de metodos para inicializar s3 e duckdb

--- historical_flights_airport_gym/soda/check_function.py
+++ historical_flights_airport_gym/soda/check_function.py
@@ -1,18 +1,50 @@
 import json
+import os
 from pathlib import Path
 
 from soda.scan import Scan
 
-from historical_flights_airport_gym.utils.aws.S3 import S3
+from historical_flights_airport_gym.utils.aws.S3 import (
+    S3ClientFactory,
+    S3Config,
+    S3Storage,
+)
 from historical_flights_airport_gym.utils.build_path import path_file_raw
-from historical_flights_airport_gym.utils.duckdb.connect_duckdb import DuckDBManager
+from historical_flights_airport_gym.utils.duckdb.connect_duckdb import (
+    DuckDBConnection,
+    DuckDBQuery,
+    DuckDBS3Configurator,
+)
 
 
-def check(event, context):
+def build_s3() -> S3Storage:
+    config = S3Config(
+        region=os.getenv("AWS_DEFAULT_REGION", "us-east-1"),
+        access_key=os.getenv("ACCESS_KEY"),
+        endpoint=os.getenv("ENDPOINT_URL"),
+        secret_access_key=os.getenv("SECRET_ACCESS_KEY"),
+    )
+    s3_client = S3ClientFactory().create(config)
+    return S3Storage(s3_client=s3_client)
+
+
+def build_conn_duckdb() -> DuckDBConnection:
+    db = DuckDBConnection()
+    conn = db.get_conn()
+    db_s3_config = DuckDBS3Configurator(conn)
+    db_s3_config.configure(s3_endpoint=os.getenv("S3_ENDPOINT"))
+    return conn
+
+
+def lambda_handler(event, context):
     bucket = event.get("bucket")
     key = event.get("key")
     table_name = event.get("table_name")
     checks_subpath = event.get("checks_subpath")
+
+    s3 = build_s3()
+    conn = build_conn_duckdb()
+    db_query = DuckDBQuery(conn=conn)
 
     root_path = str(Path(__file__).resolve().parent)
 
@@ -36,9 +68,7 @@ def check(event, context):
         );
     """
 
-    db = DuckDBManager()
-    conn = db.conn
-    db.make_query(query)
+    db_query.make_query(query=query)
 
     layer = key.split("/")[0]
 
@@ -56,7 +86,6 @@ def check(event, context):
     file_name = path_file_raw()
     key_log = f"logs/dq-checks/{layer}/{file_name}.json"
 
-    s3 = S3()
     jsonData = json.dumps(jsonObj, indent=4)
     response_s3 = s3.upload_file(bucket=bucket, data=jsonData, key=key_log)
     return {
