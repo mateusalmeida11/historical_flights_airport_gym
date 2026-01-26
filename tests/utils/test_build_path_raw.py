5e36be09f3c4b911e746e65f4198b5c14ba68cb5
Mateus Almeida <mateusalmeida881@gmail.com>
2026-01-26 16:31:54 -0300

    teste: new - validar se o path esta retornando o valor correto

--- tests/utils/test_build_path_raw.py
+++ tests/utils/test_build_path_raw.py
@@ -1,6 +1,6 @@
 import re
 
-from historical_flights_airport_gym.utils.build_path import path_file_raw
+from historical_flights_airport_gym.utils.build_path import get_root_path, path_file_raw
 
 
 def is_valid_filename(filename):
@@ -11,3 +11,11 @@ def is_valid_filename(filename):
 def test_format_file_is_correct():
     filename = path_file_raw()
     assert is_valid_filename(filename=filename)
+
+
+def test_to_find_path_root_correctly():
+    root_path = get_root_path()
+    assert (
+        str(root_path)
+        == "/Users/mateusalmeida/Desktop/projetos/historical_flights_airport_gym/historical_flights_airport_gym"
+    )
