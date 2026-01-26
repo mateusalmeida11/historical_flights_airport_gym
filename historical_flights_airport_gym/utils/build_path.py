3876500d37f4d651b7cf64445ac945123862626b
Mateus Almeida <mateusalmeida881@gmail.com>
2026-01-26 16:31:14 -0300

    feature: criacao de metodo para chegar na raiz do projeto

--- historical_flights_airport_gym/utils/build_path.py
+++ historical_flights_airport_gym/utils/build_path.py
@@ -1,5 +1,6 @@
 import datetime
 import time
+from pathlib import Path
 
 
 def path_file_raw():
@@ -7,3 +8,10 @@ def path_file_raw():
     today_str = today.strftime("%Y_%m_%d")
     timestamp_ms = int(time.time() * 1000)
     return f"{today_str}_{timestamp_ms}"
+
+
+def get_root_path(project_name="historical_flights_airport_gym"):
+    path = Path(__file__).resolve()
+    for parent in path.parents:
+        if parent.name == project_name:
+            return parent
