5463744cadc89d174b4f1fdb1398f31c3075c6a8
Mateus Almeida <mateusalmeida881@gmail.com>
2026-01-28 15:09:19 -0300

    refactor: estruturacao para injecao de dependencia

--- historical_flights_airport_gym/soda/check_function.py
+++ historical_flights_airport_gym/soda/check_function.py
@@ -1,20 +1,23 @@
 import json
 import os
-from pathlib import Path
-
-from soda.scan import Scan
 
 from historical_flights_airport_gym.utils.aws.S3 import (
     S3ClientFactory,
     S3Config,
     S3Storage,
 )
-from historical_flights_airport_gym.utils.build_path import path_file_raw
+from historical_flights_airport_gym.utils.build_path import (
+    build_path_data_quality_check_file,
+    get_root_path,
+    path_file_raw,
+)
 from historical_flights_airport_gym.utils.duckdb.connect_duckdb import (
     DuckDBConnection,
     DuckDBQuery,
     DuckDBS3Configurator,
 )
+from historical_flights_airport_gym.utils.quality.check import SodaAnalyzer
+from historical_flights_airport_gym.utils.queries.query import QualityQuerieService
 
 
 def build_s3() -> S3Storage:
@@ -37,59 +40,44 @@ def build_conn_duckdb() -> DuckDBConnection:
 
 
 def lambda_handler(event, context):
+    # 1. Extracao de parametros
     bucket = event.get("bucket")
     key = event.get("key")
     table_name = event.get("table_name")
     checks_subpath = event.get("checks_subpath")
 
+    # 2. Inicializacao
     s3 = build_s3()
     conn = build_conn_duckdb()
     db_query = DuckDBQuery(conn=conn)
 
-    root_path = str(Path(__file__).resolve().parent)
-
-    checks_path = f"{root_path}/sources/"
-
-    if checks_subpath:
-        checks_path += checks_subpath
-
+    # 3. Criacao de URI
     s3_uri = f"s3://{bucket}/{key}"
 
-    query = f"""
-    CREATE OR REPLACE VIEW '{table_name}' AS
-    SELECT
-        content.*
-    FROM
-        (
-            SELECT
-                unnest(content) AS content
-            FROM
-                read_json('{s3_uri}')
-        );
-    """
+    # 4. Criacao de Path para Quality Check
+    root_path = get_root_path()
+    checks_path = build_path_data_quality_check_file(
+        root_path=root_path, source_file=checks_subpath
+    )
+    checks_path = str(checks_path)
 
-    db_query.make_query(query=query)
+    # 5. Criacao de View
+    query_executor = QualityQuerieService(db_query)
+    query_executor.create_view_from_json_staging(table_name=table_name, s3_uri=s3_uri)
 
+    # 6. Run Soda Scan
+    soda = SodaAnalyzer(conn=conn)
+    exit_code, scan_results = soda.run_scan(checks_path=checks_path)
+
+    # 7. create path to upload s3
     layer = key.split("/")[0]
-
-    scan = Scan()
-    scan.set_verbose(True)
-    scan.add_duckdb_connection(conn)
-    scan.set_data_source_name("duckdb")
-    scan.add_sodacl_yaml_files(checks_path)
-
-    result = scan.execute()
-    jsonObj = scan.get_scan_results()
-
-    # create path to upload s3
-
     file_name = path_file_raw()
     key_log = f"logs/dq-checks/{layer}/{file_name}.json"
 
-    jsonData = json.dumps(jsonObj, indent=4)
+    jsonData = json.dumps(scan_results, indent=4)
     response_s3 = s3.upload_file(bucket=bucket, data=jsonData, key=key_log)
     return {
-        "status_soda": result,
+        "status_soda": exit_code,
         "bucket": bucket,
         "path_file_check": f"{bucket}/{key}",
         "s3": {"status_code": response_s3["ResponseMetadata"]["HTTPStatusCode"]},
